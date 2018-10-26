f6b80a722b2169dbd1ed8587f5037e3d2a8b7eb5
Jehorn <gerardgu@outlook.com>
2018-10-26 15:34:05 +0800

    rollback

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Img from './user-img-gril.jpg';
-import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack', '你好', '123'], ' ');
-    element.classList.add('hello');
-
-    // 将图片添加到div
-    var img = new Image();
-    img.src = Img;
-
-    element.appendChild(img);
-
-    console.log(Data);
 
     return element;
 }
